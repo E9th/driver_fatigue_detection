c76b4d1d66834538c96fa371328b98bed0594633
Thanapon Dongphuyaw <thanapon.dongphuyaw@gmail.com>
2025-06-23 08:54:12 +0700

    Update admin-guard.tsx

--- components/admin-guard.tsx
+++ components/admin-guard.tsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { LoadingScreen } from "./loading-screen";
 
+
 interface AdminGuardProps {
   children: React.ReactNode;
 }
